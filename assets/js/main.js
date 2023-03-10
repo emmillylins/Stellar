a0e27d81338e37515e48cf0dd33c28abf9547960
emmy <104332292+emmillylins@users.noreply.github.com>
2023-03-10 19:05:08 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,3 +121,14 @@
 		});
 
 })(jQuery);
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(serviceWorker => {
+        console.log('Service Worker registered: ' + serviceWorker);
+      })
+      .catch(error => {
+        console.log('Error registering the Service Worker: ' + error);
+      });
+  }
